48c62c514efbe0d2db57d9c81b75aa7a59095f5d
James Rogers <jarogers095@gmail.com>
2020-02-21 05:24:23 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,8 +10,8 @@ class Scraper
     return Nokogiri::HTML("http://learn-co-curriculum.github.io/site-for-scraping/courses")
   end
   
-  def get_courses(html_doc)
-    return html_doc.css("#course-grid > section > article")
+  def get_courses()
+    return get_page().html_doc.css("#course-grid > section > article")
   end
   
   def make_courses()
